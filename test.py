9181539a14f018e643d56ddd5dbd1e31041fa63c
杨萧玉 <yulingtianxia@users.noreply.github.com>
2017-05-17 00:37:46 +0800

    Update test.py

--- test.py
+++ test.py
@@ -9,12 +9,12 @@ import numpy as np
 # path to the model weights files.
 top_model_weights_path = 'fc_model.h5'
 # dimensions of our images.
-img_width, img_height = 150, 150
+img_width, img_height = 300, 300
 
 train_data_dir = 'data/train'
 validation_data_dir = 'data/validation'
-nb_train_samples = 1818
-nb_validation_samples = 199
+nb_train_samples = 1808
+nb_validation_samples = 192
 epochs = 50
 batch_size = 16
 
@@ -50,12 +50,12 @@ def save_bottlebeck_features():
 def train_top_model():
     train_data = np.load(open('bottleneck_features_train.npy'))
     train_labels = np.array(
-        [0] * 446 + [1] * 395 + [2] * 307 + [3] * 301 + [4] * 369)
+        [0] * 436 + [1] * 395 + [2] * 307 + [3] * 301 + [4] * 369)
     train_labels = to_categorical(train_labels, num_classes=5)
 
     validation_data = np.load(open('bottleneck_features_validation.npy'))
     validation_labels = np.array(
-        [0] * 33 + [1] * 30 + [2] * 60 + [3] * 34 + [4] * 42)
+        [0] * 33 + [1] * 30 + [2] * 53 + [3] * 34 + [4] * 42)
     validation_labels = to_categorical(validation_labels, num_classes=5)
 
     model = Sequential()
